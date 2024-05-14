ea06800f00e9423926eddc2ea710a9465b8b9da9
Bravista <sekretz@web.de>
2024-05-14 13:53:03 +0200

    Welcome Screen Farbe eingerichtet

--- lib/src/features/welcome/presentation/welcome_screen.dart
+++ lib/src/features/welcome/presentation/welcome_screen.dart
@@ -6,21 +6,40 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 100),
-              Image.asset("assets/images/hauptlogo.png"),
-              const TextField(),
-              const TextField(),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("login"),
-              ),
-            ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 255, 173, 114),
+                Color(0xFFE8E6DF),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              tileMode: TileMode.mirror),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 80),
+                Image.asset("assets/images/hauptlogo.png"),
+                const TextField(),
+                const TextField(),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginScreen()));
+                  },
+                  child: const Text("login"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
